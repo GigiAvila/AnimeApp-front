02fe0e83c98523de640bce25f46dce7542962716
GigiAvila <gigi.s.avila@gmail.com>
2024-03-25 22:43:47 +0100

    change Url login Image

--- src/components/Login/Signin.jsx
+++ src/components/Login/Signin.jsx
@@ -1,51 +1,56 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import {
   LoginFormBg,
   LoginImageWrapper,
   LoginImageContainer,
   LoginContainer,
-  LoginText,
+  LoginText
 } from './Signin.Styles'
-import LoginImage from '../../assets/nezuko_manga.webp'
-import LoginForm from './LoginForm';
-import RegisterButton from '../Register/RegisterButton';
-import RegisterForm from '../Register/RegisterForm';
-import LoginOption from './LoginOption';
 
-
+import LoginForm from './LoginForm'
+import RegisterButton from '../Register/RegisterButton'
+import RegisterForm from '../Register/RegisterForm'
+import LoginOption from './LoginOption'
 
 const Signin = ({ showLoginForm, setShowLoginForm }) => {
-
-const [newAccount, setNewAccount] = useState(false)
-const [isModalOpen, setIsModalOpen] = useState(false)
-
+  const [newAccount, setNewAccount] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   return showLoginForm ? (
     <LoginFormBg>
       <LoginContainer>
         <LoginText>
-          {!newAccount ?
-         <> <LoginForm isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
-          <RegisterButton setNewAccount={setNewAccount}/>
-        </>
-        : 
-        <> 
-        <RegisterForm isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} setNewAccount={setNewAccount}/>
-        <LoginOption setNewAccount={setNewAccount}/>
-      </>
-        }
+          {!newAccount ? (
+            <>
+              {' '}
+              <LoginForm
+                isModalOpen={isModalOpen}
+                setIsModalOpen={setIsModalOpen}
+              />
+              <RegisterButton setNewAccount={setNewAccount} />
+            </>
+          ) : (
+            <>
+              <RegisterForm
+                isModalOpen={isModalOpen}
+                setIsModalOpen={setIsModalOpen}
+                setNewAccount={setNewAccount}
+              />
+              <LoginOption setNewAccount={setNewAccount} />
+            </>
+          )}
         </LoginText>
-      <LoginImageContainer>
-        <LoginImageWrapper>
-          <img
-              src={LoginImage}
+        <LoginImageContainer>
+          <LoginImageWrapper>
+            <img
+              src='https://res.cloudinary.com/dqdyvyknw/image/upload/v1711402957/nezuko_manga_qgoxdf.webp'
               alt='Login Image. Nezuko from Demon Slayer Anime'
             />
           </LoginImageWrapper>
         </LoginImageContainer>
       </LoginContainer>
     </LoginFormBg>
-  ) : null;
-};
+  ) : null
+}
 
-export default Signin;
+export default Signin
